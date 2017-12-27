fc6f5af427e489e7b0d9bcab841a69dd3175ff11
Bassam <bassamabdelhamid7@gmail.com>
2017-12-27 06:40:47 +0200

    Adding sync support

--- QuranTafseerCSharpClient/Client.cs
+++ QuranTafseerCSharpClient/Client.cs
@@ -9,6 +9,7 @@ namespace QuranTafseer
     public class Client : IDisposable
     {
         System.Net.Http.HttpClient _httpClient = new System.Net.Http.HttpClient();
+        System.Net.WebClient _webClient = new System.Net.WebClient();// sync support, I know I should use something else for both, I wil
 
         //TODO: Implement more advanced cash
         Dictionary<string, string> _cash = new Dictionary<string, string>();
@@ -18,7 +19,7 @@ namespace QuranTafseer
 
         }
 
-        //TODO: Add error handling
+        //TODO: Add error handling for RequestJsonAsync
         async Task<string> RequestJsonAsync(string url)
         {
             if (_cash.ContainsKey(url)) return _cash[url];
@@ -30,6 +31,17 @@ namespace QuranTafseer
             return response;
         }
 
+        //TODO: Add error handling for RequestJson
+        string RequestJson(string url)
+        {
+            if (_cash.ContainsKey(url)) return _cash[url];
+
+            string response = _webClient.DownloadString(url);
+
+            _cash.Add(url, response);
+
+            return response;
+        }
 
         /// <summary>
         /// Get a list of Quran chapters/Sura
@@ -41,14 +53,34 @@ namespace QuranTafseer
         }
 
         /// <summary>
+        /// Get a list of Quran chapters/Sura
+        /// </summary>
+        /// <returns></returns>
+        public List<Sura> GetSuraList()
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<List<Sura>>(RequestJson(Endpoints.Chapters));
+        }
+
+        /// <summary>
         /// Get the text for specific Verse in Sura.
         /// </summary>
         /// <param name="suraNumber">The Sure number/index</param>
         /// <param name="ayahNumber">The Ayah number/index</param>
         /// <returns></returns>
-        public async Task<Verse> GetVerseDetails(int suraNumber, int ayahNumber)
+        public async Task<Verse> GetVerseDetailsAsync(int suraNumber, int ayahNumber)
         {
             return Newtonsoft.Json.JsonConvert.DeserializeObject<Verse>(await RequestJsonAsync(String.Format(Endpoints.VerseDetails, suraNumber, ayahNumber)));
+        }
+
+        /// <summary>
+        /// Get the text for specific Verse in Sura.
+        /// </summary>
+        /// <param name="suraNumber">The Sure number/index</param>
+        /// <param name="ayahNumber">The Ayah number/index</param>
+        /// <returns></returns>
+        public Verse GetVerseDetails(int suraNumber, int ayahNumber)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<Verse>(RequestJson(String.Format(Endpoints.VerseDetails, suraNumber, ayahNumber)));
         }
 
         /// <summary>
@@ -58,6 +90,15 @@ namespace QuranTafseer
         public async Task<List<Tafseer>> GetAvailableTafseersListAsync()
         {
             return Newtonsoft.Json.JsonConvert.DeserializeObject<List<Tafseer>>(await RequestJsonAsync(Endpoints.AvailableTafseers));
+        }
+
+        /// <summary>
+        /// Get a list of Quran tafseer available in Tafseer API.
+        /// </summary>
+        /// <returns></returns>
+        public List<Tafseer> GetAvailableTafseersList()
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<List<Tafseer>>(RequestJson(Endpoints.AvailableTafseers));
         }
 
         /// <summary>
@@ -76,11 +117,34 @@ namespace QuranTafseer
         /// Get a specific tasfeer for verse in chapter/Sura.
         /// </summary>
         /// <param name="tafseerId">The id of the Tafsser</param>
+        /// <param name="suraNumber">The Sure number/index</param>
+        /// <param name="ayahNumber">The Ayah number/index</param>
+        /// <returns></returns>
+        public VerseTafseer GetVerseTafseer(int tafseerId, int suraNumber, int ayahNumber)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<VerseTafseer>(RequestJson(String.Format(Endpoints.VerseTafseer, tafseerId, suraNumber, ayahNumber)));
+        }
+
+        /// <summary>
+        /// Get a specific tasfeer for verse in chapter/Sura.
+        /// </summary>
+        /// <param name="tafseerId">The id of the Tafsser</param>
         /// <param name="verse">verse object the you want tafseer for</param>
         /// <returns></returns>
         public async Task<VerseTafseer> GetVerseTafseerAsync(int tafseerId, Verse verse)
         {
             return Newtonsoft.Json.JsonConvert.DeserializeObject<VerseTafseer>(await RequestJsonAsync(String.Format(Endpoints.VerseTafseer, tafseerId, verse.SuraIndex, verse.AyahNumber)));
+        }
+
+        /// <summary>
+        /// Get a specific tasfeer for verse in chapter/Sura.
+        /// </summary>
+        /// <param name="tafseerId">The id of the Tafsser</param>
+        /// <param name="verse">verse object the you want tafseer for</param>
+        /// <returns></returns>
+        public VerseTafseer GetVerseTafseer(int tafseerId, Verse verse)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<VerseTafseer>(RequestJson(String.Format(Endpoints.VerseTafseer, tafseerId, verse.SuraIndex, verse.AyahNumber)));
         }
 
         bool disposed;
@@ -97,6 +161,7 @@ namespace QuranTafseer
             if (disposing)
             {
                 _httpClient.Dispose();
+                _webClient.Dispose();
             }
 
             disposed = true;
